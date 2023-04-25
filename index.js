f750424ee78502035d6c3c79bc9b57a0f1a90bf3
Nanaya Miki <nanayamiki@gmail.com>
2023-04-25 05:33:15 +0000

    completed ISS Spotter III

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // const { fetchMyIP } = require("./iss");
 const { fetchCoordsByIP } = require("./iss");
+const { fetchISSFlyOverTimes } = require("./iss");
 // fetchMyIP((error, ip) => {
 // 	if (error) {
 // 		console.log("It didn't work!", error);
@@ -8,12 +9,22 @@ const { fetchCoordsByIP } = require("./iss");
 
 // 	console.log("It worked! Returned IP:", ip);
 // });
-const ip = "8.8.4.4.";
+const ip = "8.8.4.4";
 fetchCoordsByIP(ip, (error, data) => {
 	if (error) {
 		console.log("it didn't work!", error);
 		return;
 	} else {
 		console.log("It worked!", data);
+	}
+});
+
+const coords = { latitude: "49.27670", longitude: "-123.13000" };
+fetchISSFlyOverTimes(coords, (error, passTimes) => {
+	if (error) {
+		console.log("didnt work", error);
+		return;
+	} else {
+		console.log("It worked!", passTimes);
 	}
 });
--- iss.js
+++ iss.js
@@ -42,7 +42,36 @@ const fetchCoordsByIP = function (ip, callback) {
 	});
 };
 
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+
+const fetchISSFlyOverTimes = function (coords, callback) {
+	const url = `https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`;
+	request(url, (error, response, body) => {
+		if (error) {
+			callback(`error: ${error}`, null);
+		}
+
+		if (response.statusCode !== 200) {
+			const message = `Success status was ${response.statusCode}. Server message says: ${body} when fetching for IP ${body.ip}`;
+			callback(Error(message), null);
+			return;
+		}
+		const responseData = JSON.parse(body).response;
+		callback(null, responseData);
+	});
+};
+
 module.exports = {
 	//fetchMyIP,
 	fetchCoordsByIP,
+	fetchISSFlyOverTimes,
 };
